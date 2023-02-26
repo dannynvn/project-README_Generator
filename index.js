2a1e4af15331613ec8d16d9ac571003d43193ca9
Danny Nguyen <dannynvn@gmail.com>
2023-02-26 15:28:50 -0800

    added question structure and prompt method into init function

--- index.js
+++ index.js
@@ -3,13 +3,48 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'test question 1',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: 'question 2',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: 'question 3',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: 'question 4',
+        name: '',
+    },
+    {
+        type: 'input',
+        message: 'question 5',
+        name: '',
+    }
+    {
+        type: 'list',
+        message: 'question 6',
+        choices: ['a', 'b', 'c'],
+        name: 'license',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions);
+}
 
 // Function call to initialize app
 init();
